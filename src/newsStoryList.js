af01a703919e637b9f4db17eba9efdc22ff13f64
Colin Scally <colin.scally@gmail.com>
2017-06-22 18:08:44 +0100

    jSON data from API is parsed and converted into individual stories

--- src/newsStoryList.js
+++ src/newsStoryList.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function NewsStoryList() {
+  this.parsed = "";
   this.list = [];
 };
 
@@ -8,5 +9,11 @@ NewsStoryList.prototype.callAPI = function (url) {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", url, false);
   xhr.send();
-  return JSON.parse(xhr.responseText);
-}
+  this.parsed = JSON.parse(xhr.responseText).response.results;
+};
+
+NewsStoryList.prototype.createList = function () {
+  for (var i=0 ; i < this.parsed.length; i++) {
+    this.list.push(new Story(this.parsed[i]));
+  };
+};
