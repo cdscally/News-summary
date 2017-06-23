9666903a1023c19e6a32841479659e0d7f099719
Colin Scally <colin.scally@gmail.com>
2017-06-23 11:21:42 +0100

    can get summary and full text from aylien API

--- src/newsStory.js
+++ src/newsStory.js
@@ -2,4 +2,14 @@ var Story = function(object) {
   this.headline = object.webTitle;
   this.URL = object.webUrl;
   this.imageURL = object.fields.thumbnail;
+  this.summary = "";
+  this.fullText = "";
 };
+
+Story.prototype.summarise = function() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", `http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=${this.URL}`, false);
+  xhr.send();
+  this.summary = JSON.parse(xhr.responseText).sentences.join(' ')
+  this.fullText = JSON.parse(xhr.responseText).text
+}
